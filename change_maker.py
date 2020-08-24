83736716a3d0508b1d1b0813454081cd57272ce4
Pete Looney <petelooney81@gmail.com>
2020-08-24 16:26:42 -0500

    finished logic for change_maker.py

--- change_maker.py
+++ change_maker.py
@@ -35,10 +35,59 @@ def is_it_number(bill_val):
             print(f'\nInvalid input, please type in a number that is larger than the total bill of ${bill_val}')
 
 # takes the payment minus the bill_val and then calls specific functions that get the bills tuple and then another that gets the coins tuple
-# def make_change(payment, bill_val):
-    # total_change = payment - bill_val
-    # bills = []
-    # coins = []
+def make_change(coin_payment, bills_payment):
+    bills = []
+    coins = []
+    bills = make_bills_change(bills_payment)
+    coins = make_coins_change(coin_payment)
+    return (bills, coins)
+
+
+def make_bills_change(bills_change):
+    # initiate an empty bills_list
+    bills_list = [0, 0, 0, 0, 0, 0]
+    while bills_change != 0:
+        if bills_change >= 100:
+            bills_list[-1] += 1
+            bills_change -= 100
+        elif bills_change >= 50:
+            bills_list[-2] += 1
+            bills_change -= 50
+        elif bills_change >= 20: 
+            bills_list[-3] += 1
+            bills_change -= 20
+        elif bills_change >= 10:
+            bills_list[-4] += 1
+            bills_change -= 10
+        elif bills_change >= 5:
+            bills_list[-5] += 1
+            bills_change -= 5
+        elif bills_change >= 1:
+            bills_list[-6] += 1
+            bills_change -= 1
+    return bills_list
+
+def make_coins_change(coins_change):
+    # initiate an empty coins_list
+    coins_list = [0, 0, 0, 0]
+    rounded_coins = round(coins_change, 2)
+    while rounded_coins != 0.00:
+        if rounded_coins >= 0.25:
+            coins_list[-1] += 1
+            rounded_coins -= 0.25
+        elif rounded_coins >= 0.10:
+            coins_list[-2] += 1
+            rounded_coins -= 0.10
+        elif rounded_coins >= 0.05:
+            coins_list[-3] += 1
+            rounded_coins -= 0.05
+        elif rounded_coins >= 0.01:
+            coins_list[-4] += 1
+            rounded_coins -= 0.01
+        elif rounded_coins < 0.01:
+            rounded_coins = 0.00
+            coins_list[-4] += 1
+    return coins_list
 
 # END FUNCTION SECTION
 #-----------------------------------------------------------------------------------------------
@@ -49,3 +98,19 @@ total_charge = round(random.random()*100, 2)
 # then we will get our user input for the payment
 print(f'\nHere is the total charge for your meal:\n${total_charge} \nHow much money would you like to pay?\nDon\'t worry we are really good at making change')
 user_input = is_it_number(total_charge)
+
+# get the total change amount
+total_change = user_input - total_charge
+
+# divides up our total_charge into the whole number (for bills_charge) and decimal number (for coins_charge) parts
+bills_charge, coins_charge = divmod(total_change, 1)
+
+# we need to round our coins charge again
+coins_charge = round(coins_charge, 2)
+
+# print(coins_charge)
+# print(bills_charge)
+# call the make_change function with the bills_charge and coins_charge as the parameters
+answer = make_change(coins_charge, bills_charge)
+
+print(answer)
